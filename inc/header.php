7dd78c98e95e2946e1ec5af4294093be81330ed9
Eamonn Mannix <eamonnmannix@Eamonns-MacBook-Pro.local>
2021-04-18 10:39:19 +0100

    favion added

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Eamonn Mannix</title>
         <meta name="description" content="Staycity aparthotel tech test - a custom form to take and display user selections">
+        <link rel="shortcut icon" href="img/favicon.ico">
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.7.0/css/all.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
